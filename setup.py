0bc50a16e8a2721007a41b9ebc145fef3e93e47a
Yury Pukhalsky <aikipooh@gmail.com>
2017-03-22 11:31:04 +0300

    Packaging

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
+
+setup(
+    author='Yury Pukhalsky',
+    author_email='aikipooh@gmail.com',
+    name='retr',
+    version='1.0',
+    description='Parallelised proxy-switching engine for scraping. Based on requests or aiohttp (alpha)',
+    long_description=convert('README.md', 'rst'),
+    url='https://github.com/aikipooh/retr',
+    # license='BSD License',
+    # platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    packages=find_packages(exclude=['example', 'docs', 'testshop']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'requests>=2.0', # For the legacy (threaded) version
+    ]
+)
