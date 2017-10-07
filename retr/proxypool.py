2ed98db00a17330fed3d8bf805b14e84bd09c335
Yury Pukhalsky <aikipooh@gmail.com>
2017-10-07 15:51:02 +0300

    rewritten to be a little slimmer.

--- retr/proxypool.py
+++ retr/proxypool.py
@@ -2,39 +2,31 @@
 
 from threading import Lock
 from logging import getLogger, ERROR
-from time import sleep
 from datetime import datetime, timedelta
 
-from requests import Session
-from lxml import etree
-
-#from .filter import filter
 from .proxypool_common import proxypool as pp_common
 
 lg=getLogger(__name__)
     
 class proxypool(pp_common):
-    '''This class contains a list of proxies, with functions to manipulate this list. With time the list is sorted by usefulness of the proxy: bad proxies go to the end, good proxies stay in the beginning. The access is thread-safe.'''
 
-    def update_replenish(self):
-        self.replenish_at=datetime.now()+timedelta(seconds=self.replenish_interval)
-
-    def __init__(self, arg=None, max_retries=-1, replenish_interval=None,
-                 replenish_threads=400, no_disable=False):
+    def __init__(self, *args, **kwargs):
         self.lock=Lock()
-        super().__init__(arg, max_retries, replenish_interval,
-                         replenish_threads, no_disable)
+        super().__init__(*args, **kwargs)
         
-    def set_status(self, p, st=None, *args):
-        with self.lock: super().set_status(p, st, *args)
+    def set_status(self, *args, **kwargs):
+        with self.lock: super().set_status(*args, **kwargs)
                 
-    def release_proxy(self, p):
-        with self.lock: super().release_proxy(p)
+    def release_proxy(self, *args):
+        with self.lock: super().release_proxy(*args)
+
+    # def active_number(self):
+    #     with self.lock: super().active_number()
 
     def get_proxy(self) -> 'proxy':
         with self.lock:
             return super().get_proxy()
             
-    def write(self, exclude_disabled=False):
+    def write(self, **kwargs):
         with self.lock:
-            super().write(self, exclude_disabled)
+            super().write(**kwargs)
